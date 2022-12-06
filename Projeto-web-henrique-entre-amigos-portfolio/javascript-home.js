3912d53b5834bbf2476f9c299433159b430e2462
Renato Gomes da Silva <96601602+renatogomesedeus@users.noreply.github.com>
2022-12-05 21:57:03 -0300

    Add files via upload

---
+++ Projeto-web-henrique-entre-amigos-portfolio/javascript-home.js
@@ -0,0 +1,29 @@
+/*header Mauricio inicio*/
+
+
+/*header Mauricio Fim*/
+
+ /*section 1 Renato inicio*/
+
+
+/*section 1 Renato Fim*/
+
+ /*section 2 Daniel inicio*/
+
+
+/*section 2 Daniel Fim*/
+
+/*section 3 Jhonas inicio*/
+
+
+/*section 3 Jhonas Fim*/
+
+ /*section 4 Henrique inicio*/
+
+
+/*section 4 Henrique Fim*/
+
+/*section 5 Daniel inicio*/
+
+
+/*section 5 Daniel Fim*/
